7b4882c6568851bad3d383ffcf57486eac44b070
heroandtn3 <heroandtn3@gmail.com>
2013-10-27 21:21:57 +0700

    first working version

--- FlickrUploader.py
+++ FlickrUploader.py
@@ -1,257 +1,72 @@
 #!/usr/bin/env python
 
-import http.client
-import hashlib
 import xml.etree.ElementTree as ET
-import urllib.parse
-import time
-import json
-import hmac
-import base64
-import mimetypes
-import os
 import requests
+import flickr_auth as auth
+import sys
 
 class FlickrApi():
     api_key = '34c6e64bd0e8a8e72e5cc6f0794d0c93'
     app_secret = '9bc9e08ac4fc7abc'
-    host = 'www.flickr.com'
     
     def __init__(self):
-        if not self.load_config():
-            self.key = self.app_secret + '&'
-            self.oauth_token, self.oauth_token_secret = self.get_request_token()
-            self.key = self.app_secret + '&' + self.oauth_token_secret
-            self.authorize()
-            self.access_token()
-            self.save_config()
-
-    def md5(self, data):
-        '''
-        Return md5 hash as string.
-        '''
-        md5 = hashlib.md5()
-        md5.update(bytes(data, 'UTF-8'))
-        return md5.hexdigest()
-
-    def sha1(self, data):
-        '''
-        Return sha1 hash as string
-        '''
-        sha1 = hashlib.sha1()
-        sha1.update(bytes(data, 'UTF-8'))
-        return sha1.hexdigest()
-
-    def gen_signature(self, method, path, parameters={}, host=host):
-        '''
-        Generate and return signature as string.
-        '''
-        base = method + '&' + urllib.parse.quote('http://' + host + path, safe='')
-        params = ''
-        for name in sorted(parameters.keys()):
-            if not isinstance(parameters.get(name), str):
-                #print(parameters.get(name))
-                continue
-            params += name + '=' + parameters[name] + '&'
-        params = urllib.parse.quote(params[:-1])
-        base += '&' + params
-        #print(base)
-        
-        hashed = hmac.new(self.key.encode(), base.encode(), hashlib.sha1).digest()
-        b = base64.encodestring(hashed)
-        b = b.rstrip()
-
-        # the signature
-        #signature = urllib.parse.quote(b)
-        signature = b.decode()
-        #print(signature)
-        return signature
-
-    def gen_url(self, path, parameters):
-        '''
-        Generate and return path with parameters as string
-        '''
-        request_url = path
-        if len(parameters) > 0:
-            request_url += '?'
-            for name in sorted(parameters.keys()):
-                request_url += name + '=' + parameters[name] + '&'
-
-            # remove last '&' character and spaces
-            request_url = request_url[:-1].replace(' ', '')
-        #print('http://' + self.host + request_url)
-        return request_url
-
-    def http_get(self, path, parameters, headers={}, host=host, oauth=False):
-        '''
-        Send HTTP GET request
-        Return response as dict by default.
-        '''
-        if oauth:
-            parameters['oauth_signature'] = self.gen_signature('GET', path, parameters, host)
-        conn = http.client.HTTPConnection(host)
-        conn.request('GET', self.gen_url(path, parameters))
-        resp = conn.getresponse()
-        format = parameters.get('format') # avoid KeyError exception
-        if format == 'json':
-            decoded_result = resp.read().decode()
-            #print(decoded_result)
-            result = json.loads(decoded_result)
-        elif oauth:
-            result = dict()
-            items = resp.read().decode().split('&')
-            for item in items:
-                name, value = item.split('=')
-                result[name] = value
-                if name == 'oauth_problem':
-                    break
-        else:
-            # default is xml
-            result = ET.fromstring(resp.read().decode())
-        return result
-
-    def http_post(self, path, parameters, headers={}, host=host, oauth=False, files=None):
-        '''
-        Send HTTP POST request
-        Return response as dict by default.
-        '''
-        #print(headers)
-        if oauth:
-            parameters['oauth_signature'] = self.gen_signature('POST', path, parameters)
-        params = urllib.parse.urlencode(parameters)
-        #print(params)
-        conn = http.client.HTTPConnection(host)
-        conn.request('POST', path, params, headers)
-        resp = conn.getresponse()
-        print(resp.status, resp.reason)
-        return resp.read()
-
-    def get_request_token(self):
-        path = '/services/oauth/request_token'
-        parameters = dict()
-        parameters['oauth_nonce'] = str(int(time.time()))
-        parameters['oauth_timestamp'] = str(int(time.time()))
-        parameters['oauth_consumer_key'] = self.api_key
-        parameters['oauth_signature_method'] = 'HMAC-SHA1'
-        parameters['oauth_version'] = '1.0'
-        parameters['oauth_callback'] = 'http%3A%2F%2Fwww.example.com'
-        #parameters['oauth_signature'] = self.gen_signature('GET', path, parameters)
-        result = self.http_get(path, parameters, oauth=True)
-        if result.get('oauth_callback_confirmed') == 'true':
-            self.oauth_token = result['oauth_token']
-            self.oauth_token_secret = result['oauth_token_secret']
-            return self.oauth_token, self.oauth_token_secret
-        else:
-            return result
-
-    def authorize(self):
-        path = '/services/oauth/authorize'
-        parameters = dict()
-        parameters['oauth_token'] = self.oauth_token
-        parameters['perms'] = 'write'
-        link = 'http://' + self.host + self.gen_url(path, parameters)
-        print('Open this link with your browse to confirm:', link)
-        oauth_verifier = input('Paste you verifier here: ')
-        self.oauth_verifier = str(oauth_verifier).replace(' ', '')
-
-    def access_token(self):
-        path = '/services/oauth/access_token'
-        parameters = dict()
-        parameters['oauth_nonce'] = str(int(time.time()))
-        parameters['oauth_timestamp'] = str(int(time.time()))
-        parameters['oauth_verifier'] = self.oauth_verifier
-        parameters['oauth_consumer_key'] = self.api_key
-        parameters['oauth_signature_method'] = 'HMAC-SHA1'
-        parameters['oauth_version'] = '1.0'
-        parameters['oauth_token'] = self.oauth_token
-        #parameters['oauth_signature'] = self.gen_signature('GET', path, parameters)
-        self.http_get(path, parameters, oauth=True)
-        #print(result)
-
-    def save_config(self):
-        config = dict()
-        config['oauth_token'] = self.oauth_token
-        config['oauth_token_secret'] = self.oauth_token_secret
-        config['oauth_verifier'] = self.oauth_verifier
-        f = open('flickr.cfg', 'w')
-        f.write(json.dumps(config))
-
-    def load_config(self):
-        try:
-            f = open('flickr.cfg')
-            data = ''
-            while True:
-                line = f.readline()
-                if len(line) == 0:
-                    break
-                data += line
-            config = json.loads(data)
-            self.oauth_token = config['oauth_token']
-            self.oauth_token_secret = config['oauth_token_secret']
-            self.oauth_verifier = config['oauth_verifier']
-            self.key = self.app_secret + '&' + self.oauth_token_secret
-        except:
-            return False
-        return True
+        auth.do_oauth()
 
     def test_login(self):
-        host = 'api.flickr.com'
-        path = '/services/rest'
+        url = 'http://api.flickr.com/services/rest'
         parameters = dict()
         parameters['nojsoncallback'] = '1'
-        parameters['oauth_nonce'] = str(int(time.time()))
-        parameters['oauth_timestamp'] = str(int(time.time()))
-        parameters['oauth_consumer_key'] = self.api_key
-        parameters['oauth_verifier'] = self.oauth_verifier
-        parameters['api_key'] = self.api_key
-        parameters['oauth_signature_method'] = 'HMAC-SHA1'
-        parameters['oauth_version'] = '1.0'
-        parameters['oauth_token'] = self.oauth_token
         parameters['format'] = 'json'
         parameters['method'] = 'flickr.test.login'
-        print(self.http_get(path, parameters, host=host, oauth=True))
+        parameters = auth.gen_oauth_params('GET', url, parameters)
+        resp = requests.get(url, params=parameters)
+        print(resp.text)
 
     def get_upload_status(self):
-        host = 'api.flickr.com'
-        path = '/services/rest'
+        url = 'http://api.flickr.com/services/rest'
         parameters = dict()
         parameters['nojsoncallback'] = '1'
-        parameters['oauth_nonce'] = str(int(time.time()))
-        parameters['oauth_timestamp'] = str(int(time.time()))
-        parameters['oauth_consumer_key'] = self.api_key
-        parameters['oauth_verifier'] = self.oauth_verifier
-        #parameters['api_key'] = self.api_key
-        parameters['oauth_signature_method'] = 'HMAC-SHA1'
-        parameters['oauth_version'] = '1.0'
-        parameters['oauth_token'] = self.oauth_token
         parameters['format'] = 'json'
         parameters['method'] = 'flickr.people.getUploadStatus'
-        print(self.http_get(path, parameters, host=host, oauth=True))
+        parameters = auth.gen_oauth_params('GET', url, parameters)
+        resp = requests.get(url, params=parameters)
+        print(resp.text)
 
     def upload_photo(self, file_name):
         url = 'http://up.flickr.com/services/upload'
-        #url = 'http://requestb.in/wkgdufwk'
         files = {'photo': open(file_name, 'rb')}
+        params = dict()
+        params['is_public'] = '0'
+        params = auth.gen_oauth_params('POST', url, params)
+        resp = requests.post(url, files=files, data=params)
+        dom = ET.fromstring(resp.text)
+        if dom.get('stat') == 'ok':
+            photoid = dom.find('photoid').text
+            self.get_photo_links(photoid)
+        else:
+            print("There's something wrong here")
 
-        parameters = dict()
-        parameters['oauth_signature_method'] = 'HMAC-SHA1'
-        parameters['oauth_timestamp'] = str(int(time.time()))
-        parameters['oauth_nonce'] = str(int(time.time()))
-        parameters['oauth_consumer_key'] = self.api_key
-        parameters['oauth_token'] = self.oauth_token
-        parameters['oauth_version'] = '1.0'
-        parameters['oauth_verifier'] = self.oauth_verifier
-        parameters['oauth_signature'] = self.gen_signature(
-            'POST', 
-            '/services/upload', 
-            parameters=parameters,
-            host='up.flickr.com')
-
-        r = requests.post(url, files=files, data=parameters)
-        print(r.text)
+    def get_photo_links(self, photoid):
+        url = 'http://api.flickr.com/services/rest'
+        params = dict()
+        params['api_key'] = self.api_key
+        params['photo_id'] = photoid
+        params['method'] = 'flickr.photos.getSizes'
+        params = auth.gen_oauth_params('GET', url, params)
+        resp = requests.get(url, params=params)
+        dom = ET.fromstring(resp.text)
+        if dom.get('stat') == 'ok':
+            sizes = dom.find('sizes').findall('size')
+            for size in sizes:
+                print('%12s: %s' % (size.get('label'), size.get('source')))
+        else:
+            print("There's something wrong here")
+        
 
 if __name__ == '__main__':
     api = FlickrApi()
-    #api.upload_photo('image.jpg')
-    api.test_login()
+    photo = sys.argv[1]
+    api.upload_photo(photo)
+    #api.test_login()
     #api.get_upload_status()
+    #api.get_photo_links('10513318854')
---
+++ flickr_auth.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+
+import time
+import urllib.parse
+import base64
+import hmac
+import hashlib
+import requests
+import json
+
+api_key = '34c6e64bd0e8a8e72e5cc6f0794d0c93'
+api_secret = '9bc9e08ac4fc7abc'
+key = api_secret + '&'
+oauth_token = ''
+oauth_token_secret = ''
+oauth_verifier = ''
+
+def get_oauth_token():
+    '''
+    If success, get oauth_token and oauth_token_secret.
+    '''
+    global key
+    global oauth_token
+    global oauth_token_secret
+    url = 'http://www.flickr.com/services/oauth/request_token'
+    #url = 'http://requestb.in/1acqk8s1'
+    params = {'oauth_callback': 'http://www.example.com'}
+    params = _gen_unoauth_params('GET', url, params)
+    resp = requests.get(url, params=params)
+    r = resp.text
+    if 'oauth_callback_confirmed=true' in r:
+        pairs = r.split('&')
+        oauth_token = pairs[1].split('=')[1]
+        oauth_token_secret = pairs[2].split('=')[1]
+        key += oauth_token_secret
+
+def authorize():
+    '''
+    Authorize permission with user account.
+    '''
+    global oauth_verifier
+    url = 'http://www.flickr.com/services/oauth/authorize'
+    url += '?oauth_token={0}&perms=write'.format(oauth_token)
+    print('Open this link with your browse:', url)
+    line = input('Paste your oauth_verifier here: ')
+    oauth_verifier = line.strip()
+
+def access_token():
+    global oauth_verifier, oauth_token, oauth_token_secret
+    url = 'http://www.flickr.com/services/oauth/access_token'
+    params = {
+        'oauth_token': oauth_token,
+        'oauth_verifier': oauth_verifier
+    }
+    params = _gen_unoauth_params('GET', url, params)
+    resp = requests.get(url, params=params)
+    fields = resp.text.split('&')
+    oauth_token = fields[1].split('=')[1]
+    oauth_token_secret = fields[2].split('=')[1]
+
+
+def _gen_unoauth_params(method, url, params={}):
+    params['oauth_nonce'] = str(int(time.time()))
+    params['oauth_timestamp'] = str(int(time.time()))
+    params['oauth_consumer_key'] = api_key
+    params['oauth_signature_method'] = 'HMAC-SHA1'
+    params['oauth_version'] = '1.0'
+    params['oauth_signature'] = _gen_signature(method, url, params)
+    return params
+
+def _gen_signature(method, url, parameters={}):
+    '''
+    Generate and return signature as string.
+    '''
+    base = method + '&' + urllib.parse.quote(url, safe='')
+    params = ''
+    for name in sorted(parameters.keys()):
+        params += urllib.parse.quote(name) + '=' + urllib.parse.quote(parameters[name], safe='') + '&'
+    params = urllib.parse.quote(params[:-1], safe='')
+    base += '&' + params
+    #print(base)
+    
+    hashed = hmac.new(key.encode(), base.encode(), hashlib.sha1).digest()
+    b = base64.encodestring(hashed)
+    b = b.rstrip()
+
+    # the signature
+    signature = b.decode()
+    #print(signature)
+    return signature
+
+
+def save_config():
+        config = dict()
+        config['oauth_token'] = oauth_token
+        config['oauth_token_secret'] = oauth_token_secret
+        config['oauth_verifier'] = oauth_verifier
+        f = open('flickr.cfg', 'w')
+        f.write(json.dumps(config))
+
+def load_config(filename='flickr.cfg'):
+    try:
+        f = open(filename)
+        data = ''
+        while True:
+            line = f.readline()
+            if len(line) == 0:
+                break
+            data += line
+        config = json.loads(data)
+        global oauth_token, oauth_token_secret, oauth_verifier, key
+        oauth_token = config['oauth_token']
+        oauth_token_secret = config['oauth_token_secret']
+        oauth_verifier = config['oauth_verifier']
+        key = api_secret + '&' + oauth_token_secret
+        f.close()
+        return True
+    except:
+        f.close()
+        return False
+
+def gen_config():
+    get_oauth_token()
+    authorize()
+    access_token()
+    save_config()
+
+def gen_oauth_params(method, url, params={}):
+    params['oauth_token'] = oauth_token
+    params = _gen_unoauth_params(method, url, params)
+    return params
+
+def do_oauth():
+    if not load_config():
+        gen_config()
+
+if __name__ == '__main__':
+    do_oauth()
